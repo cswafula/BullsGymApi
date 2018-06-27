bbe25293af8fe6d197aebf50ea049dc9def04087
BlackAssasin <charleswafula29@gmail.com>
2018-06-27 19:24:43 +0300

    First Commit

---
+++ app/Http/Controllers/CredentialsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ Credentials;
+
+class CredentialsController extends Controller
+{
+    public function Login($Email,$Pass){
+        $User=Credentials::all()->where("Email",$Email)->where("Password",$Pass);
+        return $User->toArray();
+    }
+    public function Register(){
+        $Register=new Register();
+        $Register->Email=request('Email');
+        $Register->Password=request('Password');
+        $Register->Gender=request('Gender');
+        $Register->Weight=request('Weight');
+        $Register->BMI=request('BMI');
+        $Register->GymName=request('GymName');
+        $Register->Latitude=request('Latitude');
+        $Register->Longitude=request('Longitude');
+        $Register->ImageURL=request('ImageURL');
+        $Register->save();
+    }
+}
---
+++ database/migrations/2018_06_27_115025_create_credentialstable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCredentialstable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('Credentials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Email');
+            $table->string('Password');
+            $table->string('Gender');
+            $table->string('Weight');
+            $table->string('BMI');
+            $table->string('GymName');
+            $table->string('Latitude');
+            $table->string('Longitude');
+            $table->string('ImageURL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::dropIfExists('Credentials');
+    }
+}
