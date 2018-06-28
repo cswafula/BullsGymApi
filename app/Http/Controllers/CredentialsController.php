eee3dc6e51c3501f1f8e9f26f9ca6493be21c1f9
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-06-28 23:38:43 +0300

    Update CredentialsController.php

--- app/Http/Controllers/CredentialsController.php
+++ app/Http/Controllers/CredentialsController.php
@@ -9,7 +9,7 @@ class CredentialsController extends Controller
 {
     public function Login($Email,$Pass){
         $User=Credentials::all()->where("Email",$Email)->where("Password",$Pass);
-        return $User->toJson();
+        return Response::Json($User);
     }
     public function Register(){
         $Register=new Credentials();
