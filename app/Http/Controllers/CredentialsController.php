cc1e6d60fc9b49d965354bb665e18a0c5729798e
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-06-29 18:50:37 +0300

    Update CredentialsController.php

--- app/Http/Controllers/CredentialsController.php
+++ app/Http/Controllers/CredentialsController.php
@@ -9,9 +9,23 @@ class CredentialsController extends Controller
 {
     public function Login($Email,$Pass){
         $User=Credentials::all()->where("Email",$Email)->where("Password",$Pass);
-        $json_array=array();
-        $json_array[]=$User;
-        return $json_array;
+        foreach($User as $Fetched){
+        $Results[0]=$Fetched->Email;
+        $Results[1]=$Fetched->Password;
+        $Results[2]=$Fetched->Gender;
+        $Results[3]=$Fetched->Weight;
+        $Results[4]=$Fetched->BMI;
+        $Results[5]=$Fetched->GymName;
+        $Results[6]=$Fetched->Latitude;
+        $Results[7]=$Fetched->Longitude;
+        $Results[8]=$Fetched->ImageURL;
+        
+        if($Results == null){
+            return "Empty";
+        }else{
+        return json_encode(['User'=>$Results]);   
+        }
+        }
     }
     public function Register(){
         $Register=new Credentials();
