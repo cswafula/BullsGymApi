66c63b90d75d36965c38a0a3b99de3eff6f144c9
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-05 15:28:20 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/Register','CredentialsController@Register');
 Route::post('/Update/{Email}','CredentialsController@UpdateProfile');
 Route::get('/Login/{Email}/{Pass}','CredentialsController@Login');
+Route::get('/UserWorkouts/{Email}','WorkoutsController@FetchWorkouts');
+
