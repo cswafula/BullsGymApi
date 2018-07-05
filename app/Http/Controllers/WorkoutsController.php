619d65378d6d9ac65ec06695c09115b8da999496
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-05 15:33:34 +0300

    Update WorkoutsController.php

--- app/Http/Controllers/WorkoutsController.php
+++ app/Http/Controllers/WorkoutsController.php
@@ -7,5 +7,18 @@ use App\Workouts;
 
 class WorkoutsController extends Controller
 {
-    //
+    public function FetchWorkouts($Email){
+    $User=Workouts::all()->where("Email",$Email);
+    return json_encode(['Workouts'=>$User]);
+    }
+    
+    public function InsertWorkout(){ 
+        $Workout=new Workouts();
+        $Workout->Email=request('Email');
+        $Workout->WorkoutName=request('WorkoutName');
+        $Workout->Description=request('Description');
+        $Workout->Reps=request('Reps');
+        $Workout->Date=request('Date');
+        $Workout->save();
+    }
 }
