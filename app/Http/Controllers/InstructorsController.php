48e0a36719d06ddaf0cd0699cc3151730368cf53
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-06 00:44:40 +0300

    Add files via upload

---
+++ app/Http/Controllers/InstructorsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Instructors;
+
+class InstructorsController extends Controller
+{
+    public function Fetch(){
+        $User=Instructors::all();
+        return json_encode($User);
+    }
+}
