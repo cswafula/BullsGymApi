b66db95883c2462430429faaeab69cfd9cf9b20e
Charles Shitanda Wafula <39024494+charleswafula29@users.noreply.github.com>
2018-07-05 21:50:48 +0300

    Update WorkoutsController.php

--- app/Http/Controllers/WorkoutsController.php
+++ app/Http/Controllers/WorkoutsController.php
@@ -9,13 +9,6 @@ class WorkoutsController extends Controller
 {
     public function FetchWorkouts($Email){
     $User=Workouts::all()->where("Email",$Email);
-     foreach($User as $Fetched){
-        $Results[0]=$Fetched->Email;
-        $Results[1]=$Fetched->WorkoutName;
-        $Results[2]=$Fetched->Description;
-        $Results[3]=$Fetched->Reps;
-        $Results[4]=$Fetched->Date;   
-        }
     return json_encode(['Workouts'=>$Results]);
     }
     
